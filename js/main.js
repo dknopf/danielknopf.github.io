dc61397ce29b1f2dfad81585c91eedc6386fdfd5
Daniel Knopf <danielkn609@gmail.com>
2021-07-27 17:04:16 -0400

    finished adding dropdown arrows to experiences

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(".collapsible-btn").click(function () {
   $("i", this).toggleClass("lnr-chevron-right lnr-chevron-down");
 });
+
+$(showCollapsible());
+
+$(window).on("resize", function () {
+  showCollapsible();
+});
+
+function showCollapsible() {
+  if ($(window).width() > 500) {
+    console.log($(window).height());
+    $(".collapsible-text").addClass("collapse");
+  } else {
+    console.log("TEXT");
+    $(".collapsible-text").removeClass("collapse");
+  }
+}
